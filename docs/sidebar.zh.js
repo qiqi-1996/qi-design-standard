3fe2910b1a7f7838d25e34a53b0e9a7d762a0081
SaekiRaku <guoqiqi@qiqi1996.com>
2020-08-17 21:44:32 +0800

    update

--- docs/sidebar.zh.js
+++ docs/sidebar.zh.js
@@ -7,4 +7,17 @@ var SidebarZH = [
         title: "项目与开发计划",
         link: "/zh/schedule"
     },
+    {
+        title: "设计规范",
+        children: [
+            {
+                title: "色彩",
+                link: "/zh/standard/colors.md"
+            }
+        ]
+    },
+    {
+        title: "组件样式规范",
+        children: []
+    },
 ]
